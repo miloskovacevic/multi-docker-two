31749bb408712f325ddf217bf19b1e41fb9693da
Miloš Kovačević <miloscpt@gmail.com>
2019-01-24 22:35:36 +0100

    Initial

---
+++ client/src/OtherPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default () => {
+    return (
+        <div>
+            other page
+            <Link to="/">Go back home </Link>
+        </div>
+    )
+};
+
+
+
+
+
+
+
+
+
+
+
